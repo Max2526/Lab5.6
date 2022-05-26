5412be60572d4416612e6359e0ac5e3569742a8a
username <skulamaksym@gmail.com>
2022-05-26 18:33:22 +0300

    lab

---
+++ src/com/company/Project1.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Project1 {
+
+    public static void main(String[] args) {
+        int i = 57;
+        int q = 8;
+        System.out.println(i/q);
+        System.out.println(i%q);
+
+
+    }
+}
